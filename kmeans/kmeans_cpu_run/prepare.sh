2303ae9c2d2abf08fe7acbf52aea96c6ba9089c6
YIWEI-CHEN <feberium@gmail.com>
2015-06-27 12:46:16 +0800

    fix prepare.sh

--- kmeans/kmeans_cpu_run/prepare.sh
+++ kmeans/kmeans_cpu_run/prepare.sh
@@ -2,8 +2,10 @@ hdfs dfs -mkdir -p kmeans/input
 hdfs dfs -mkdir -p kmeans/centroids
 if [[ $HOSTNAME == "pasX" ]]; then
     hdfs dfs -put /mnt/data/project/hadoop-input/kmeans/* kmeans/input
-elif [[ $HOSTNAME == "pasH[0-9]" ]]; then
+#    echo "in pasX"
+elif [[ -n `echo $HOSTNAME | grep "pasH[0-9]"` ]]; then
     hdfs dfs -put /home/`whoami`/hadoop_input/kmeans/* kmeans/input
+#    echo "in pasX[0-9]"
 fi
 hdfs dfs -cp kmeans/input/part0 kmeans/centroids
 hdfs dfs -ls kmeans/
